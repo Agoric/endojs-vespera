leotm/endo

feat(cli): Add import-bundle0 command

packages/cli/src/endo.js
@@ -533,6 +533,34 @@ export const main = async rawArgs => {
       }
     });
 
+  program
+    .command('import-bundle0 <worker> <readableBundleName>')
+    .option(
+      '-n,--name <name>',
+      'Assigns a name to the result for future reference, persisted between restarts',
+    )
+    .action(async (worker, readableBundleName, cmd) => {
+      const { name: resultPetName } = cmd.opts();
+      const { getBootstrap } = await provideEndoClient(
+        'cli',
+        sockPath,
+        cancelled,
+      );
+      try {
+        const bootstrap = getBootstrap();
+        const workerRef = E(bootstrap).provide(worker);
+
+        const result = await E(workerRef).importBundle0(
+          readableBundleName,
+          resultPetName,
+        );
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+        cancel(error);
+      }
+    });
+
   // Throw an error instead of exiting directly.
   program.exitOverride();
 

Agoric/endojs-vespera

packages/cli/src/endo.js
@@ -535,6 +535,34 @@ export const main = async rawArgs => {
       }
     });
 
+  program
+    .command('import-bundle0 <worker> <readableBundleName>')
+    .option(
+      '-n,--name <name>',
+      'Assigns a name to the result for future reference, persisted between restarts',
+    )
+    .action(async (worker, readableBundleName, cmd) => {
+      const { name: resultName } = cmd.opts();
+      const { getBootstrap } = await provideEndoClient(
+        'cli',
+        sockPath,
+        cancelled,
+      );
+      try {
+        const bootstrap = getBootstrap();
+        const workerRef = E(bootstrap).provide(worker);
+
+        const result = await E(workerRef).importBundle0(
+          readableBundleName,
+          resultName,
+        );
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+        cancel(error);
+      }
+    });
+
   // Throw an error instead of exiting directly.
   program.exitOverride();
 